9ffef340499bdf2ea4b158ca586d8d342c795d84
ara shiho <backjlutv.1107@gmail.com>
2026-09-17 21:44:07 +0900

    Initial commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>自己紹介</title>
+</head>
+<body>
+    <h1>プロフィール</h1>
+    <p>名前：山田太郎</p>
+    <p>年齢：25歳</p>
+    <p>趣味：</p>
+    <ul>
+        <li>プログラミング</li>
+        <li>読書</li>
+        <li>旅行</li>
+    </ul>
+</body>
+</html>
